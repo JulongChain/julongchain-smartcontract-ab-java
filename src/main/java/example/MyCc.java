78d1d6fef6844a6c29f8ef4384da64dca5d7213a
wanliangbing <wlb@dxct.org>
2018-06-05 10:00:30 +0800

    [BJC-137]

--- src/main/java/example/MyCc.java
+++ src/main/java/example/MyCc.java
@@ -13,9 +13,15 @@
  */
 package example;
 
-import shim.ISmartContract;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import shim.ISmartContractStub;
 import shim.SmartContractBase;
+
+import java.util.List;
+
+import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * 类描述
@@ -26,20 +32,92 @@ import shim.SmartContractBase;
  */
 public class MyCc extends SmartContractBase {
 
+  private static Log log = LogFactory.getLog(MyCc.class);
+
   @Override
   public SmartContractResponse init(ISmartContractStub stub) {
-    System.out.println("mcc init");
+    log.info("=================================================>>>> myCc init");
+    List<byte[]> args = stub.getArgs();
+    log.info(args.toString());
+    log.info("args size:" + args.size());
+    for (int i = 0; i < args.size(); i++) {
+      System.out.println("args" + i + ": " + new String(args.get(i)));
+    }
+    stub.putState("a", "100".getBytes());
+    stub.putState("b", "200".getBytes());
     return newSuccessResponse();
   }
 
   @Override
   public SmartContractResponse invoke(ISmartContractStub stub) {
-    System.out.println("mcc invoke");
+    log.info("=================================================>>>> myCc invoke");
+    List<byte[]> args1 = stub.getArgs();
+    log.info(args.toString());
+    log.info("args size:" + args1.size());
+
+    final String function = stub.getFunction();
+    final String[] args = stub.getParameters().toArray(new String[0]);
+
+    switch (function) {
+      case "invoke":
+        return invoke(stub, args);
+      case "query":
+        return query(stub, args);
+      default:
+        return newErrorResponse(format("未知方法: %s", function));
+    }
+
     return newSuccessResponse();
   }
 
   @Override
   public String getSmartContractStrDescription() {
     return "MyCC";
+  }
+
+  // 转账
+  private SmartContractResponse invoke(ISmartContractStub stub, String[] args) {
+    if (args.length != 3) throw new IllegalArgumentException("参数错误");
+    // 转账，Ａ转账给Ｂ，金额Ｃ
+    final String fromKey = args[0]; // A
+    final String toKey = args[1]; // B
+    final String amount = args[2]; // C
+
+    // 获取身份信息
+    final String fromKeyState = stub.getStringState(fromKey);
+    final String toKeyState = stub.getStringState(toKey);
+
+    // 转账人余额获取类型转换
+    Double fromAccountBalance = Double.parseDouble(fromKeyState);
+    Double toAccountBalance = Double.parseDouble(toKeyState);
+
+    // 转账金额类型转换
+    Double transferAmount = Double.parseDouble(amount);
+
+    // 确保金额足够
+    if (transferAmount > fromAccountBalance) {
+      throw new IllegalArgumentException("资金不足");
+    }
+
+    // 转账操作
+    log.info(format("转账人：%s 转 %f 元给转账人：%s", fromKey, transferAmount, toKey));
+    Double newFromAccountBalance = fromAccountBalance - transferAmount;
+    Double newToAccountBalance = toAccountBalance + transferAmount;
+    log.info(
+        format(
+            "各自余额为: %s = %f, %s = %f", fromKey, newFromAccountBalance, toKey, newToAccountBalance));
+    stub.putStringState(fromKey, Double.toString(newFromAccountBalance));
+    stub.putStringState(toKey, Double.toString(newToAccountBalance));
+    log.info("转账结束.");
+
+    return newSuccessResponse(format("成功转账 %f ,", transferAmount));
+  }
+
+  // 查询
+  private SmartContractResponse query(ISmartContractStub stub, String[] args) {
+    if (args.length != 1) throw new IllegalArgumentException("参数错误");
+    final String accountKey = args[0];
+    double amount = Double.parseDouble(stub.getStringState(accountKey));
+    return newSuccessResponse(format("成功查询 Name:" + accountKey + ", Amount:" + amount));
   }
 }
